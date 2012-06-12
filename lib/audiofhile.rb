25ddd3c9c4d3f48ead77c5f28fe481559b1d05ca
Ryan Houston <ryanhouston83@gmail.com>
2012-06-11 22:50:25 -0400

    Add a couple basic tasks to the cli

---
+++ lib/audiofhile.rb
@@ -0,0 +1,4 @@
+module Audiofhile
+  autoload :Collection,     'audiofhile/collection'
+  autoload :FileFinder,     'audiofhile/file_finder'
+end
--- lib/audiofhile/cli.rb
+++ lib/audiofhile/cli.rb
@@ -3,9 +3,21 @@ require "thor"
 module Audiofhile
   class CLI < Thor
 
+    class_option "path", :type => "string", :required => true, :banner => "The path to the audio collection"
+
+    def initialize(*)
+      super
+      @collection = Collection.new(options['path'])
+    end
+
     desc "files", "List all files in the audio collection"
     def files
-      puts "This is the 'files' job"
+      puts @collection.audio_files
+    end
+
+    desc "formats", "List all audio file formats in the audio collection"
+    def formats
+      puts @collection.formats
     end
 
   end
