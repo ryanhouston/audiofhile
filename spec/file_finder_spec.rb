147ef59d199d53f1f34cd98a9b8d66763bf4817f
Ryan Houston <ryanhouston83@gmail.com>
2012-07-29 21:44:33 -0400

    Fill in some file finder specs

--- spec/file_finder_spec.rb
+++ spec/file_finder_spec.rb
@@ -7,9 +7,21 @@ module Audiofhile
 
     its(:audio_files) { should be_a(Array) }
 
-    it "Raises an error for unknown formats" do
-      expect { subject.file_pattern_match(:doc) }.to raise_error(InvalidAudioFormatError)
+    it "raises an error for unknown formats" do
+      expect do
+         subject.file_pattern_match(:doc)
+      end.to raise_error(InvalidAudioFormatError)
     end
+
+    it "builds a file search pattern to match extensions" do
+      subject.file_pattern_match(:mp3).should eq('*.mp3')
+    end
+
+    it "responds whether or not a extension is valid" do
+      expect { subject.is_valid_extension(:mp3).should be_true }
+      expect { subject.is_valid_extension(:nope).should be_false }
+    end
+
   end
 end
 
