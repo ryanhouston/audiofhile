7af9c3859a9017f672c40faf968825a5ff90c999
Ryan Houston <ryanhouston83@gmail.com>
2012-08-07 09:17:50 -0400

    Add ability to set persistent path
    Using the 'path' command will write the path to the collection to a
    hidden config file which can be used for future operations. This
    prevents the user from having the provide the --path options for every
    command they wish to run.

--- lib/audiofhile/cli.rb
+++ lib/audiofhile/cli.rb
@@ -1,13 +1,30 @@
 require "thor"
+require "yaml"
 
 module Audiofhile
   class CLI < Thor
+    CONFIG_FILE = File.join(File.dirname(__FILE__), '..', '..', '.audiofhile').freeze
 
     class_option "path", :type => "string", :required => true, :banner => "The path to the audio collection"
 
     def initialize(*)
       super
       @collection = Collection.new(options['path'])
+    end
+
+    desc "path [DIR]", "Show or set the path to be used in future operations"
+    def path(dir = nil)
+      if dir
+        options = {"audiofhile" => {"collection" => {"path" => dir } } }
+        File.open(CONFIG_FILE, 'w+') {|f| f.write(options.to_yaml) }
+      else
+        unless File.exists? CONFIG_FILE
+          raise RuntimeError, "No collection path has been set"
+        end
+
+        config = YAML::load(CONFIG_FILE)
+        puts config['audiofhile']['collection']['path']
+      end
     end
 
     desc "files", "List all files in the audio collection"
